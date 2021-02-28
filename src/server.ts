528961ae5fd4aa66314a68115163ee13112d78bf
Lucas <lucas.ramuh@gmail.com>
2021-02-28 13:57:31 -0300

    Inclusão do Banco de Dados com TypeORM e controller da entidade Users

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,13 @@
+import 'reflect-metadata';
 import express from 'express';
+import './database';
+import { router } from './routes';
+
 
 const app = express();
 
-/**
-*
-*  GET => Busca
-*  POST => Salvar
-*  PUT => Alterar
-*  DELETE => Deletar
-*  PATCH => Alteração específica
-* 
-*/
-
-app.get("/", (request, response) =>{
-    return response.json({message: "Hello World - NLW4"});
-});
-
-app.post("/", (resquest, response) => {
-    //recebeu dados para salvar
-
-    return response.json({message: "Os dados foram salvos com sucesso!"});
-});
-
-
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
 
